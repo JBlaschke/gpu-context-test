3c5c6af7927775305c512df4d55876e13f667071
Johannes Blaschke <jpblaschke@lbl.gov>
2021-02-07 21:13:01 -0800

    add set-device example

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ NVCC     := nvcc
 INCLUDE  := -I.
 
 all: test_PrimaryCtx_0.ex test_PrimaryCtx_all.ex set_DeviceCtx_all.ex\
-    set_DeviceCtx_0.ex get_DeviceCtx_0.ex
+    set_DeviceCtx_0.ex get_DeviceCtx_0.ex test_SetDevice_all.ex\
+    test_SetDevice_0.ex
 
 %.ex: %.o util.o
 	$(NVCC) -o $@ -lcuda $^
---
+++ test_SetDevice_all.cu
@@ -0,0 +1,30 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <util.h>
+
+
+
+int main (int argc, char * argv[]) {
+
+    int n_dev;
+    cudaGetDeviceCount(& n_dev);
+
+    printf("%d devices are available\n", n_dev);
+    if (n_dev <= 0) return 1;
+
+
+    printf("Checking state of primary context -- before first CUDA runtime API call\n");
+    check_primary_ctx(n_dev);
+
+
+    printf("Checking state of primary context -- after a cudaSetDevice on each device\n");
+
+    for (int i=0; i<n_dev; i++) {
+        cudaSetDevice(i);
+    }
+    cudaSetDevice(0);
+
+    check_primary_ctx(n_dev);
+
+    return 0;
+}
